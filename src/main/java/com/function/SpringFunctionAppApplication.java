afb5c9e1c51187820ab57112ce9f8d3b6aff7808
nilangekar-ni3 <nilangekar.ni3@gmail.com>
2022-07-26 12:35:22 +0530

    Initial Commit

---
+++ src/main/java/com/function/SpringFunctionAppApplication.java
@@ -0,0 +1,13 @@
+package com.function;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFunctionAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFunctionAppApplication.class, args);
+	}
+
+}
